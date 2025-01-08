50439b29735b618f1b07c9a2e1b2d1d18897976a
Kursat Kilic <kilic_kursat@hotmail.com>
2025-01-08 09:22:25 +0100

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-
-# Upload file
-uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+import plotly.subplots as sp
+import plotly.graph_objects as go
 
 @st.cache_data
 def load_data(file):
     df = pd.read_csv(file, sep=';', parse_dates=['ts(utc)'])
     df.rename(columns={'ts(utc)': 'Timestamp'}, inplace=True)
     return df
+
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
 if uploaded_file is not None:
     df = load_data(uploaded_file)
@@ -18,44 +18,22 @@ else:
     st.warning("Please upload a CSV file.")
     st.stop()
 
-# Select parameters
 parameters = st.multiselect(
     'Choose machine parameters to analyze and visualize:',
     options=df.columns[1:],
     default=df.columns[1]
 )
 
-# Descriptive statistics
 if parameters:
     st.subheader('Descriptive Statistics')
     stats = df[parameters].describe()
     st.write(stats)
-else:
-    st.warning('Please select at least one parameter.')
-
-# Plot options
-plot_type = st.selectbox(
-    'Select plot type:',
-    options=['Line Plot', 'Scatter Plot']
-)
-
-x_axis = st.selectbox(
-    'Select x-axis:',
-    options=['Timestamp']
-)
-
-# Generate plot
-if plot_type and parameters:
-    if plot_type == 'Line Plot':
-        fig = px.line(df, x=x_axis, y=parameters, title='Parameter Trends Over Time')
-    elif plot_type == 'Scatter Plot':
-        fig = px.scatter(df, x=x_axis, y=parameters, title='Parameter Scatter Plot Over Time')
+    
+    fig = sp.make_subplots(rows=len(parameters), cols=1, shared_xaxes=True)
+    for i, sensor in enumerate(parameters, start=1):
+        fig.add_trace(go.Scatter(x=df['Timestamp'], y=df[sensor], name=sensor, mode='lines'), row=i, col=1)
+        fig.update_yaxes(title_text=sensor, row=i, col=1)
+    fig.update_layout(height=200*len(parameters), title='Sensor Data Over Time')
     st.plotly_chart(fig)
 else:
-    st.warning('Please select plot type and parameters.')
-
-# Instructions
-st.write("### Instructions")
-st.write("1. Upload your CSV file using the uploader.")
-st.write("2. Select the parameters you want to analyze and visualize.")
-st.write("3. Choose the plot type and x-axis for visualization.")
+    st.warning('Please select at least one parameter.')
